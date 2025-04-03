2cf622d65ea22a4d3bb8a1a42ac374bc3f845617
sarkadol <sarka.dolezalova123@gmail.com>
2025-04-03 15:43:12 +0200

    language detection

--- docker/postgres_db.py
+++ docker/postgres_db.py
@@ -85,13 +85,13 @@ begin
     documents.embedding,
     documents.metadata
   from documents
---   where 1 - (documents.embedding <=> query_embedding) > similarity_threshold
-  where documents.dataset_id = any(query_dataset_ids)
+  where 1 - (documents.embedding <=> query_embedding) > similarity_threshold
+    and documents.dataset_id = any(query_dataset_ids)
     and (query_user_id is null or query_user_id = documents.user_id)
 --     and (documents.metadata->>metadata_field = metadata_value)
 --     and (metadata_field is null or documents.metadata->>metadata_field = metadata_value) -- filter by metadata
 --     and documents.metadata->>'lang' = 'cz'
-    and documents.metadata ? 'lang'
+--     and documents.metadata.lang = metadata_value
   order by documents.embedding <=> query_embedding;
 end;
 $$;"""
