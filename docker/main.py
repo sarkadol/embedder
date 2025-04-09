771245ba01115fe7de358b1bbff2a06b01a495eb
sarkadol <sarka.dolezalova123@gmail.com>
2025-04-09 08:38:38 +0200

    new embedder

--- docker/main.py
+++ docker/main.py
@@ -16,8 +16,8 @@ app = (
     get_app(settings)
    # .use_embedder(OpenAI(os.environ.get('OPENAI_APIKEY'), os.environ.get('OPENAI_ORGANISATION')))
     .use_embedder(CustomEmbedder(api_url="https://vllm.ai.e-infra.cz/v1/embeddings"))
-    #.use_db(Postgres(conn_str=os.environ.get('POSTGRES_URL'), dimensions=os.environ.get('DIMENSIONS')))
-    .use_db(Postgres(conn_str=os.environ.get('POSTGRES_URL')))
+    .use_db(Postgres(conn_str=os.environ.get('POSTGRES_URL'), dimensions=os.environ.get('DIMENSIONS')))
+    #.use_db(Postgres(conn_str=os.environ.get('POSTGRES_URL')))
 ).run()
 
 @app.exception_handler(Exception)
